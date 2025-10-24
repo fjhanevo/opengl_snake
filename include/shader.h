9e6d73bb066fc22f8aa04852a003408fc940bed3
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-24 22:23:35 +0200

    changed to camelcase

--- include/shader.h
+++ include/shader.h
@@ -8,7 +8,7 @@ class Shader
 {
 public:
     // program ID
-    unsigned int ID;
+    unsigned int m_ID;
 
     // constructor
     Shader() { }
@@ -20,18 +20,18 @@ public:
     void compile(std::string_view vertexSource, std::string_view fragmentSource);
 
     // utility uniform functions
-    void set_int(std::string_view name, int value, bool useShader = false);
-    void set_float(std::string_view name, float value, bool useShader = false);
+    void setInt(std::string_view name, int value, bool useShader = false);
+    void setFloat(std::string_view name, float value, bool useShader = false);
 
-    void set_vec2(std::string_view name, glm::vec2 &vec, bool useShader = false);
-    void set_vec3(std::string_view name, glm::vec3 &vec, bool useShader = false);
-    void set_vec4(std::string_view name, glm::vec4 &vec, bool useShader = false);
+    void setVec2(std::string_view name, glm::vec2 &vec, bool useShader = false);
+    void setVec3(std::string_view name, glm::vec3 &vec, bool useShader = false);
+    void setVec4(std::string_view name, glm::vec4 &vec, bool useShader = false);
 
-    void set_mat2(std::string_view name, glm::mat2 &mat, bool useShader = false);
-    void set_mat3(std::string_view name, glm::mat3 &mat, bool useShader = false);
-    void set_mat4(std::string_view name, glm::mat4 &mat, bool useShader = false);
+    void setMat2(std::string_view name, glm::mat2 &mat, bool useShader = false);
+    void setMat3(std::string_view name, glm::mat3 &mat, bool useShader = false);
+    void setMat4(std::string_view name, glm::mat4 &mat, bool useShader = false);
 private:
-    void check_compile_errors(unsigned int shader, std::string type);
+    void checkCompileErrors(unsigned int shader, std::string type);
 
 };
 
