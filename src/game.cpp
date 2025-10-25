94c69acb27f833d71cfce48d0cf15358394c29ad
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-25 22:55:20 +0200

    implemented more functions

---
+++ src/game.cpp
@@ -0,0 +1,199 @@
+#include "game.h"
+#include "config.h"
+#include "constants.h"
+#include "sprite_renderer.h"
+#include "resource_manager.h"
+
+#include <GLFW/glfw3.h>
+#include <filesystem>
+#include <glm/gtc/matrix_transform.hpp>
+#include <random>
+
+// helper function to get path for resources
+static std::filesystem::path getPath(const std::string &relativePath)
+{
+    return std::filesystem::path(RESOURCE_DIR) / relativePath;
+}
+
+// ----- Random number generation -----
+std::mt19937 dev;
+std::random_device r;
+std::uniform_int_distribution<> dist_x(1, Constants::SCREEN_WIDTH  / Constants::GRID_SIZE - 2);
+std::uniform_int_distribution<> dist_y(1, Constants::SCREEN_HEIGHT / Constants::GRID_SIZE - 2);
+
+// ----- Global variables ------
+SpriteRenderer *Renderer {};
+
+Game::Game(GLuint width, GLuint height)
+    : m_width(width), m_height(height), m_score(0), m_moveTimer(0.0f),
+    m_keys(), m_keysProcessed(), m_window(nullptr), m_state(GAME_ACTIVE),
+    m_snake(nullptr), m_food(nullptr), m_border(nullptr)
+{}
+
+Game::~Game()
+{
+    delete Renderer; 
+    delete m_snake;
+    delete m_food;
+    delete m_border;
+    ResourceManager::clear();
+}
+
+void Game::init()
+{
+    // ----- Load shaders -----
+    ResourceManager::loadShader(getPath("shaders/sprite.vert"), getPath("shaders/sprite.frag"), "sprite");
+    // ----- Configure shaders -----
+    glm::mat4 projection { glm::ortho(
+        0.0f,
+        static_cast<GLfloat>(m_width),
+        static_cast<GLfloat>(m_height),
+        0.0f,
+        -1.0f,
+        1.0f
+    )};
+    ResourceManager::getShader("sprite").use().setInt("sprite", 0);
+    ResourceManager::getShader("sprite").use().setMat4("projection", projection);
+
+    // ----- Load textures ----- 
+    ResourceManager::loadTexture(getPath("textures/snake_head.png"), true, "snakeHead");
+    ResourceManager::loadTexture(getPath("textures/snake_body.png"), true, "snakeBody");
+    ResourceManager::loadTexture(getPath("textures/snake_tail.png"), true, "snakeTail");
+    ResourceManager::loadTexture(getPath("textures/apple.png"), true, "apple");
+    ResourceManager::loadTexture(getPath("textures/fence.png"), true, "fence");
+    ResourceManager::loadTexture(getPath("textures/grass.png"), true, "grass");
+
+    Renderer = new SpriteRenderer(ResourceManager::getShader("sprite"));
+    
+    m_snake = new Snake(m_width, m_height);
+    m_food = new Food();
+    m_border = new Border(m_width, m_height);
+}   
+
+void Game::update(float dt)
+{
+    constexpr GLfloat MOVE_INTERVAL { 0.15f };
+    if (m_state == GAME_ACTIVE)
+    {
+        m_moveTimer += dt;
+
+        // check if enough time has passed to move the snake
+        if (m_moveTimer >= MOVE_INTERVAL)
+        {
+            Direction nextDir {m_snake->getNextDirection()};
+            m_snake->setDirection(nextDir);
+            m_snake->move();
+            m_moveTimer = 0.0f;
+        }
+
+        m_food->update(dt);
+        checkCollision();
+
+        if (!m_food->getState())
+        {
+            addFood();
+        }
+    }
+
+}
+
+void Game::playAgain()
+{
+
+}
+
+// Temporary function to draw background
+static void fillBackground(SpriteRenderer &renderer)
+{
+    for (GLuint x { 0 }; x < Constants::SCREEN_WIDTH; x += Constants::GRID_SIZE)
+    {
+        for (GLuint y { 0 }; y < Constants::SCREEN_HEIGHT; y += Constants::GRID_SIZE)
+        {
+            renderer.drawSprite(
+                ResourceManager::getTexture("grass"),
+                glm::vec2(x,y)
+            );
+        }
+    }
+}
+
+void Game::render()
+{
+    if (m_state == GAME_ACTIVE) 
+    {
+        fillBackground(*Renderer);
+        m_border->draw(*Renderer);
+        m_snake->draw(*Renderer);
+
+        if (m_food->getState())
+            m_food->draw(*Renderer);
+    }
+}
+
+void Game::processInput()
+{
+    if (m_state == GAME_ACTIVE) 
+    {
+        Direction newDir = m_snake->getNextDirection();
+        if (m_keys[GLFW_KEY_W]) newDir = Direction::UP;
+        if (m_keys[GLFW_KEY_A]) newDir = Direction::LEFT;
+        if (m_keys[GLFW_KEY_S]) newDir = Direction::DOWN;
+        if (m_keys[GLFW_KEY_D]) newDir = Direction::RIGHT;
+
+        Direction currentDir = m_snake->getCurrentDirection();
+        // prevent reversing the direction
+        if (!((currentDir == Direction::UP   && newDir == Direction::DOWN)  ||
+             (currentDir == Direction::DOWN  && newDir == Direction::UP)    ||
+             (currentDir == Direction::LEFT  && newDir == Direction::RIGHT) ||
+             (currentDir == Direction::RIGHT && newDir == Direction::LEFT)))
+        {
+            m_snake->queueDirection(newDir);
+        } 
+    }
+    if (m_state == GAME_LOST)
+    {
+        if (m_keys[GLFW_KEY_Y] && !m_keysProcessed[GLFW_KEY_Y]) 
+        {
+            playAgain();
+            m_keysProcessed[GLFW_KEY_Y] = true;
+        }
+        if (m_keys[GLFW_KEY_N] && !m_keysProcessed[GLFW_KEY_N]) 
+        {
+            glfwSetWindowShouldClose(m_window, true);
+            m_keysProcessed[GLFW_KEY_N] = true;
+        }
+    }
+}
+
+void Game::checkCollision()
+{
+
+}
+
+void Game::addFood()
+/* 
+    Randomly generates a position for Food to be added.
+    Makes sure the Food is not added on top of the Snake.
+*/
+{
+    bool isValid { false }; 
+    glm::vec2 pos{};
+    while (!isValid)
+    {
+        pos.x = dist_x(r) * Constants::GRID_SIZE;
+        pos.y = dist_y(r) * Constants::GRID_SIZE;
+
+        // assume position is valid, check it doesn't spawn on snake
+        isValid = true;
+
+        for (const auto &segment : m_snake->getSegments())
+        {
+            if (segment.x == pos.x && segment.y == pos.y)
+            {
+                isValid = false;
+                break;    
+            }
+        }
+    }
+    m_food->init(pos);
+}
