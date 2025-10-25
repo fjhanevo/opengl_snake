50f3517ed145d2ba629dc4eaa0b288cd9f7142c5
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-25 13:21:02 +0200

    changed type, added GRID_SIZE

--- include/constants.h
+++ include/constants.h
@@ -1,8 +1,9 @@
 #pragma once
+#include <glad/glad.h>
 
 namespace Constants
 {
-    constexpr int SCREEN_WIDTH = 800; 
-    constexpr int SCREEN_HEIGHT = 600; 
-
+    constexpr GLuint SCREEN_WIDTH  = 800; 
+    constexpr GLuint SCREEN_HEIGHT = 600; 
+    constexpr GLuint GRID_SIZE     = 40;
 }
