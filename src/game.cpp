13cf7d25186756b293b6fde0d09ae57d9ccd9705
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-26 14:24:38 +0100

    added playAgain, added implementations for GAME_LOST state

--- src/game.cpp
+++ src/game.cpp
@@ -3,11 +3,13 @@
 #include "constants.h"
 #include "sprite_renderer.h"
 #include "resource_manager.h"
+#include "text_renderer.h"
 
 #include <GLFW/glfw3.h>
 #include <filesystem>
 #include <glm/gtc/matrix_transform.hpp>
 #include <random>
+#include <sstream>
 
 // helper function to get path for resources
 static std::filesystem::path getPath(const std::string &relativePath)
@@ -22,17 +24,19 @@ std::uniform_int_distribution<> dist_x(1, Constants::SCREEN_WIDTH  / Constants::
 std::uniform_int_distribution<> dist_y(1, Constants::SCREEN_HEIGHT / Constants::GRID_SIZE - 2);
 
 // ----- Global variables ------
-SpriteRenderer *Renderer {};
+SpriteRenderer *Renderer{};
+TextRenderer *Text{};
 
-Game::Game(GLuint width, GLuint height)
+Game::Game(GLuint width, GLuint height, GLFWwindow *window)
     : m_width(width), m_height(height), m_score(0), m_moveTimer(0.0f),
-    m_keys(), m_keysProcessed(), m_window(nullptr), m_state(GAME_ACTIVE),
+    m_keys(), m_keysProcessed(), m_window(window), m_state(GAME_ACTIVE),
     m_snake(nullptr), m_food(nullptr), m_border(nullptr)
 {}
 
 Game::~Game()
 {
     delete Renderer; 
+    delete Text;
     delete m_snake;
     delete m_food;
     delete m_border;
@@ -64,6 +68,8 @@ void Game::init()
     ResourceManager::loadTexture(getPath("textures/grass.png"), true, "grass");
 
     Renderer = new SpriteRenderer(ResourceManager::getShader("sprite"));
+    Text = new TextRenderer(m_width, m_height);
+    Text->load(getPath("fonts/slkscr.ttf"), 24);
     
     m_snake = new Snake(m_width, m_height);
     m_food = new Food();
@@ -97,10 +103,7 @@ void Game::update(float dt)
 
 }
 
-void Game::playAgain()
-{
 
-}
 
 // Temporary function to draw background
 static void fillBackground(SpriteRenderer &renderer)
@@ -119,14 +122,35 @@ static void fillBackground(SpriteRenderer &renderer)
 
 void Game::render()
 {
+    std::stringstream ss; ss << m_score;
     if (m_state == GAME_ACTIVE) 
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         fillBackground(*Renderer);
         m_border->draw(*Renderer);
         m_snake->draw(*Renderer);
 
         if (m_food->getState())
             m_food->draw(*Renderer);
+
+        Text->renderText("Score:"+ss.str(), Constants::GRID_SIZE, Constants::GRID_SIZE, 1.0f);
+    }
+
+    if (m_state == GAME_LOST)
+    {
+        Text->renderText(
+            "Your Score:"+ss.str(), 
+            floor(Constants::SCREEN_WIDTH / (2.0f * Constants::GRID_SIZE)) * Constants::GRID_SIZE,
+            floor(Constants::SCREEN_HEIGHT/ (2.0f * Constants::GRID_SIZE)) * Constants::GRID_SIZE,
+            1.0
+        );
+        
+        Text->renderText(
+            "Play Again [y/N]",
+            floor(Constants::SCREEN_WIDTH / (2.0f * Constants::GRID_SIZE)) * Constants::GRID_SIZE,
+            floor(Constants::SCREEN_HEIGHT / (2.0f * Constants::GRID_SIZE)) * 2*Constants::GRID_SIZE,
+            1.0
+        );
     }
 }
 
@@ -225,4 +249,11 @@ void Game::addFood()
         }
     }
     m_food->init(pos);
+}
+
+void Game::playAgain()
+{
+    m_score = 0;
+    m_snake->init();
+    m_state = GAME_ACTIVE;
 }
