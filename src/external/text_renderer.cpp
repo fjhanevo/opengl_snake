eb02df271c6cd7a98ab0f95d936caa6a483efe25
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-27 19:11:21 +0100

    changed to GL_NEAREST

--- src/external/text_renderer.cpp
+++ src/external/text_renderer.cpp
@@ -87,8 +87,8 @@ void TextRenderer::load(const std::string &font, GLuint fontSize)
         // set texture options
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // Store Characters for later use
         Character character{
