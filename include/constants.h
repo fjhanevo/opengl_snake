470114facd1b8651ef3801ce027301c449bd2e8c
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-29 18:35:35 +0100

    added ROWS and COLS as normalization constants

--- include/constants.h
+++ include/constants.h
@@ -3,7 +3,10 @@
 
 namespace Constants
 {
-    constexpr GLuint SCREEN_WIDTH  = 800; 
-    constexpr GLuint SCREEN_HEIGHT = 600; 
-    constexpr GLuint GRID_SIZE     = 40;
+    constexpr GLuint SCREEN_WIDTH   { 800 }; 
+    constexpr GLuint SCREEN_HEIGHT  { 600 }; 
+    constexpr GLuint GRID_SIZE      { 40 };
+    // Normalization constants
+    constexpr GLuint ROWS           { SCREEN_WIDTH / GRID_SIZE };
+    constexpr GLuint COLS           { SCREEN_HEIGHT / GRID_SIZE };
 }
