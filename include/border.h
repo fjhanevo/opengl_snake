534ef172da9bfe9c8625e11a60bd85f09d4d6db1
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-30 20:45:51 +0100

    added m_openGrids

--- include/border.h
+++ include/border.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "sprite_renderer.h"
+#include <vector>
 
 class Border
 {
 public:
     Border(GLuint width, GLuint height);
     void draw(SpriteRenderer &renderer);
+    std::vector<glm::vec2> m_openGrids{};
 private:
     GLuint m_width{}, m_height{};
 };
