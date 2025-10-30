19ad9b8eaf542c377cce6a51f4fe8bc30d5c6494
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-30 20:45:41 +0100

    added a temp. implementation for openGrids

--- src/border.cpp
+++ src/border.cpp
@@ -4,7 +4,37 @@
 
 Border::Border(GLuint width, GLuint height)
     : m_width(width), m_height(height)
-{}
+{
+    // Top and bottom positions
+    for (GLuint x {1}; x < Constants::ROWS - 1; ++x)
+    {
+        GLfloat xCenter { x * Constants::GRID_SIZE + Constants::GRID_SIZE / 2.0f };
+        if (x % 2 == 0) // add 2 openings if even 
+        {
+            m_openGrids.emplace_back(xCenter - Constants::GRID_SIZE / 4.0f, Constants::GRID_SIZE / 2.0f);
+            m_openGrids.emplace_back(xCenter - Constants::GRID_SIZE / 4.0f, m_height - Constants::GRID_SIZE / 2.0f);
+        }
+        else 
+        {
+            m_openGrids.emplace_back(xCenter, Constants::GRID_SIZE / 2.0f);
+        }
+    }
+
+    for (GLuint y {1}; y < Constants::COLS- 1; ++y)
+    {
+        GLfloat yCenter { y * Constants::GRID_SIZE + Constants::GRID_SIZE / 2.0f };
+        if (y % 2 == 0) // add 2 openings if even 
+        {
+            m_openGrids.emplace_back(Constants::GRID_SIZE / 2.0f, yCenter - Constants::GRID_SIZE / 4.0f);
+            m_openGrids.emplace_back(m_width - Constants::GRID_SIZE / 2.0f, yCenter + Constants::GRID_SIZE);
+        }
+        else 
+        {
+            m_openGrids.emplace_back(Constants::GRID_SIZE / 2.0f, yCenter);
+        }
+    }
+
+}
 
 void Border::draw(SpriteRenderer &renderer)
 {
