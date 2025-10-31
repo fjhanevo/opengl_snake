cf2f6628e3e0a50966c032ba0a267ba1d2ebcf3b
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-31 14:57:49 +0100

    initialized m_openGrids in constructor, reworked draw to use normalized constants

--- src/border.cpp
+++ src/border.cpp
@@ -2,38 +2,50 @@
 #include "constants.h"
 #include "resource_manager.h"
 
-Border::Border(GLuint width, GLuint height)
-    : m_width(width), m_height(height)
+Border::Border()
 {
-    // Top and bottom positions
-    for (GLuint x {1}; x < Constants::ROWS - 1; ++x)
+    // clear the vector  
+    m_openGrids.clear();
+
+    // Check if rows (width) is even or odd
+    if (Constants::ROWS % 2 == 0)
     {
-        GLfloat xCenter { x * Constants::GRID_SIZE + Constants::GRID_SIZE / 2.0f };
-        if (x % 2 == 0) // add 2 openings if even 
-        {
-            m_openGrids.emplace_back(xCenter - Constants::GRID_SIZE / 4.0f, Constants::GRID_SIZE / 2.0f);
-            m_openGrids.emplace_back(xCenter - Constants::GRID_SIZE / 4.0f, m_height - Constants::GRID_SIZE / 2.0f);
-        }
-        else 
-        {
-            m_openGrids.emplace_back(xCenter, Constants::GRID_SIZE / 2.0f);
-        }
+        // add two open positions since it's even
+        GLint xCenter1 { (Constants::ROWS / 2) - 1 };
+        GLint xCenter2 { Constants::ROWS / 2 };
+
+        m_openGrids.emplace_back(xCenter1, 0);                      // Open Top position 1
+        m_openGrids.emplace_back(xCenter2, 0);                      // Open Top position 2
+        m_openGrids.emplace_back(xCenter1, Constants::COLS - 1);    // Open Bottom position 1
+        m_openGrids.emplace_back(xCenter2, Constants::COLS - 1);    // Open Bottom position 2
     }
 
-    for (GLuint y {1}; y < Constants::COLS- 1; ++y)
+    else 
     {
-        GLfloat yCenter { y * Constants::GRID_SIZE + Constants::GRID_SIZE / 2.0f };
-        if (y % 2 == 0) // add 2 openings if even 
-        {
-            m_openGrids.emplace_back(Constants::GRID_SIZE / 2.0f, yCenter - Constants::GRID_SIZE / 4.0f);
-            m_openGrids.emplace_back(m_width - Constants::GRID_SIZE / 2.0f, yCenter + Constants::GRID_SIZE);
-        }
-        else 
-        {
-            m_openGrids.emplace_back(Constants::GRID_SIZE / 2.0f, yCenter);
-        }
+        GLint xCenter { Constants::ROWS / 2 };
+        m_openGrids.emplace_back(xCenter, 0);                       // Open Top position
+        m_openGrids.emplace_back(xCenter, Constants::COLS - 1);     // Open Bottom position
     }
 
+    // check if columns (height) is even or odd
+    if (Constants::COLS % 2 == 0)
+    {
+        GLint yCenter1 { (Constants::COLS / 2) - 1 };
+        GLint yCenter2 { (Constants::COLS / 2) };
+
+        m_openGrids.emplace_back(0, yCenter1);                      // Open Left position 1
+        m_openGrids.emplace_back(0, yCenter2);                      // Open Left position 2
+        m_openGrids.emplace_back(Constants::ROWS - 1, yCenter1);    // Open Left position 1
+        m_openGrids.emplace_back(Constants::ROWS - 1, yCenter2);    // Open Left position 2
+    }
+
+    else 
+    {
+        GLint yCenter {Constants::COLS / 2};
+
+        m_openGrids.emplace_back(0, yCenter);                       // Open Left position
+        m_openGrids.emplace_back(Constants::ROWS - 1, yCenter);     // Open Left position
+    }
 }
 
 void Border::draw(SpriteRenderer &renderer)
@@ -42,14 +54,14 @@ void Border::draw(SpriteRenderer &renderer)
     {
         // Draw top border
         renderer.drawSprite(
-            (x == 9 || x == 10) ? ResourceManager::getTexture("background0") : ResourceManager::getTexture("fence"),
+            (isOpen({x, 0})) ? ResourceManager::getTexture("background0") : ResourceManager::getTexture("fence"),
             glm::vec2(x * Constants::GRID_SIZE, 0.0f)
         );
 
         // Draw bottom border
         renderer.drawSprite(
-            (x == 9 || x == 10) ? ResourceManager::getTexture("background0") : ResourceManager::getTexture("fence"),
-            glm::vec2(x * Constants::GRID_SIZE, m_height - Constants::GRID_SIZE),
+            (isOpen({x, Constants::COLS - 1})) ? ResourceManager::getTexture("background0") : ResourceManager::getTexture("fence"),
+            glm::vec2(x * Constants::GRID_SIZE, (Constants::COLS - 1) * Constants::GRID_SIZE),
             180.0f
         );
 
@@ -59,16 +71,26 @@ void Border::draw(SpriteRenderer &renderer)
     {
         // Left border column
         renderer.drawSprite(
-            (y == 7) ? ResourceManager::getTexture("background0") : ResourceManager::getTexture("fence"),
+            (isOpen({0,y})) ? ResourceManager::getTexture("background0") : ResourceManager::getTexture("fence"),
             glm::vec2(0, y * Constants::GRID_SIZE),
             270.0f
         );
 
         // Right border column
         renderer.drawSprite(
-            ResourceManager::getTexture("fence"),
-            glm::vec2(m_width - Constants::GRID_SIZE, y * Constants::GRID_SIZE),
+            (isOpen({(Constants::ROWS - 1), y})) ? ResourceManager::getTexture("background0") : ResourceManager::getTexture("fence"),
+            glm::vec2((Constants::ROWS -1)* Constants::GRID_SIZE, y * Constants::GRID_SIZE),
             90.0f 
         );
     }
+}
+
+bool Border::isOpen(const glm::vec2& gridPos) const
+{
+    for (const auto& openPos : m_openGrids)
+    {
+        if (openPos == gridPos)
+            return true;
+    }
+    return false;
 }
